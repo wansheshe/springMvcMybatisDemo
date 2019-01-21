415018f19063798d4fd4c921507d104fec130fc2
ou.shiyang <13039430100@163.com>
2019-01-21 12:02:04 +0800

    张三分

--- Jpa/src/com/osy/entity/Customer.java
+++ Jpa/src/com/osy/entity/Customer.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
-* @author 作者 E-mail11ddd111是滴爱的、ddddddwwww:
+* @author 作者 E-mail11ddd111是滴爱的、ddddddwwww张三分:
 * @version 创建时间：2019年1月16日 上午10:17:09
 * 类说明
 */
